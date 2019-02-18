1a66ad604d535045c2456c5f9bcf45240b393240
Sherwyne <sherwyne123+work@gmail.com>
2019-02-18 09:52:16 +0800

    Set home to Profile.java

---
+++ app/src/main/java/tnt_codefest/diaeta/Profile/Profile.java
@@ -0,0 +1,15 @@
+package tnt_codefest.diaeta.Profile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import tnt_codefest.diaeta.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
